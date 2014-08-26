ec467c78e4570584f72c4b2a9443152af8e2355d
Rasmus Steinke <rasi@xssn.at>
2014-08-26 21:36:28 +0200

    fixed musiclist

--- musiclist_stars.py
+++ musiclist_stars.py
@@ -1,23 +1,28 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+
 #from __future__ import print_function
+import sys
 import os
 import operator
 import glob
 import re
 import mpd  # pacman -S python2-mpd
 from functools import reduce
+
 MUSIC_ROOT = '/mnt/wasteland/Audio/Rips'
-RATING_FILE = 'rating.txt'
+
+# Queries MPD for items
 def query(c, *items, **conditions):
     if conditions:
         # turn dict into flattened list, e.g.
         # {k1: v1, k2: v2} -> [k1, v1, k2, v2]
-#        cond = reduce(operator.add, conditions.items())
+#       cond = reduce(operator.add, conditions.items())
         cond = reduce(operator.add, list(conditions.items()))
         results = c.find(*cond)
     else:
         results = c.listallinfo()
+
     yielded = set()
     for r in results:
         d = []
@@ -25,37 +30,37 @@ def query(c, *items, **conditions):
             it = r.get(i)
             if isinstance(it, list):
                 it = it[0]
+
             d.append(it)
+
         d = tuple(d)
         if all(d) and d not in yielded:
             yield d
         yielded.add(d)
+
 def get_artists(c):
     a = [r[0] for r in query(c, 'albumartist')]
     return a
+
 def get_rating(c, artist, date, album):
     r = query(c, 'file', albumartist=artist, date=date, album=album)
     path = next(r)[0]
     path = os.path.join(MUSIC_ROOT, path)
     path = os.path.dirname(path)
-    for file in os.listdir(MUSIC_ROOT):
-        if file.endswith(".ratings"):
-            RATING_FILE=file
-
+    # CD divider dirs do not hold the ratings, their parent does.
     if re.search('CD ?\d', path, flags=re.IGNORECASE):
         path = os.path.dirname(path)
-    path = os.path.join(path, RATING_FILE)
-    try:
-        with(open(path)) as f:
-            line = f.readline().strip()
-            while line:
-                if re.match("album_rating", line):
-                    return line[line.find("=")+1:]
-                line = f.readline()
-            return "-"
-    except IOError:
-        return "-"
 
+    for file in os.listdir(path):
+        if file.endswith(".ratings"):
+            path = os.path.join(path, file)
+            with(open(path)) as f:
+                line = f.readline().strip()
+                while line:
+                    if re.match("album_rating", line):
+                        return line[line.find("=")+1:]
+                    line = f.readline()
+            return "-"
 
 def main():
     c = mpd.MPDClient()
@@ -64,21 +69,23 @@ def main():
     pw = None
     if "@" in host:
         pw, host = host.split("@")
+
     c.connect(host, port)
     if pw is not None:
         c.password(pw)
+
     print('''
     <html>
     <head>
     <meta charset="utf-8"/>
     <link rel="stylesheet" href="http://netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
     <style>
-        body{background:#eee}
-        table{
-        margin:5em auto;
-        max-width:56em;
-        background: white;
-        }
+            body{background:#eee}
+            table{
+            margin:5em auto;
+            max-width:56em;
+            background: white;
+            }
     </style>
     <link href="http://maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
     </head>
@@ -95,15 +102,21 @@ def main():
     </thead>
     <tbody>
     ''')
+
     for artist in sorted(get_artists(c), key=lambda v: (v.upper(), v[0].islower())):
         albums = sorted(query(c, 'date', 'album', albumartist=artist))
         print('<tr class="newartist"><td rowspan="{0}">{1}</td>'.format(len(albums), artist))
         for i, (date, album) in enumerate(albums):
             rating = get_rating(c, artist, date, album)
-            if rating == '-':
+            if rating == '-' or rating == None:
                 output = ''
             else:
                 parts = str(rating).split('/')
+                if not parts[0].isdigit():
+                    print('Error: rating number isn\'nt numeric: {0}'.format(rating))
+                    print('Occured at {0}--{1}--{2}'.format(artist, album, date))
+                    sys.exit(1)
+
                 rate = int(parts[0])
                 max_rate = int(parts[1]) / 2
                 output = ''
@@ -114,7 +127,9 @@ def main():
                     rate = float(rate / 2)
                     first = int(str(rate).split('.')[0])
                     output += (first * '<i class="fa fa-star"></i>') + '<i class="fa fa-star-half-o"></i>'
+
                 output += int(max_rate - rate) * '<i class="fa fa-star-o"></i>'
+
             # templating libaries ftw
             print('''
             <td>{date}</td>
@@ -122,6 +137,7 @@ def main():
             <td><font color="black">{output}</font></td>
             </tr>
             '''.format(date=date, album=album, output=output))
+
     print('''
     </tbody>
     </table>
@@ -129,5 +145,8 @@ def main():
     </body>
     </html>
     ''')
+
 if __name__ == '__main__':
     main()
+
+#  vim: set ts=4 sw=4 tw=80 et :
