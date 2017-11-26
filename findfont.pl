2a1aefd358dcfae9a435e33b840fcaae5bcdfd88
Rasmus Steinke <rasi@xssn.at>
2017-11-26 12:40:23 +0100

    add pod

--- findfont.pl
+++ findfont.pl
@@ -3,23 +3,21 @@
 use utf8;
 use autodie;
 use Data::Printer;
-use Encode qw/decode/;
+use Encode qw/decode encode/;
 use Font::FreeType;
 use charnames ();
+use Pod::Usage qw(pod2usage);
 
 my @font_list = map { (split /:/, $_)[0] } `fc-list`;
 @font_list = grep(/\.(ttf|odf)$/i, @font_list);
 
 sub print_fonts {
-    my ($char) = @_;
-    my $char_code_hex = sprintf "%04x", ord decode('UTF-8', $char);
-    my $char_code_dec = sprintf "%04d", ord decode('UTF-8', $char);
-    my $char_name = charnames::viacode(hex($char_code_hex));
+    my ($char, $hex, $char_name) = @_;
+    $char = encode('UTF-8', $char);
 
     print STDERR "Glyph:   $char\n";
     print STDERR "Name:    $char_name\n";
-    print STDERR "Hex:     $char_code_hex\n";
-    print STDERR "Decimal: $char_code_dec\n\n";
+    print STDERR "Hex:     $hex\n\n";
 
     foreach my $font (@font_list) {
         my $face = Font::FreeType->new->face($font);
@@ -32,21 +30,57 @@ sub print_fonts {
     }
 }
 
-sub print_glyph {
+sub get_vars_from_hex {
     binmode(STDOUT, ":utf8");
-    my ($code) = @_;
-    my $glyph = chr(hex($code));
-    system('findfont.pl', '--glyph', $glyph);
+    my ($hex) = @_;
+    my $glyph = chr(hex($hex));
+    my $char_name = charnames::viacode($hex);
+    return($glyph, $hex, $char_name);
 }
 
+sub get_vars_from_glyph {
+    my ($glyph) = @_;
+    my $hex = sprintf "%04x", ord decode('UTF-8', $glyph);
+    my $char_name = charnames::viacode(hex($hex));
+    return($glyph, $hex, $char_name);
+}
+    
 sub main {
+    my ($glyph, $hex, $char_name);
     my ($mode, $input) = @_;
-    if($mode eq "--code") {
-        print_glyph($input);
+
+    if($mode eq "--hex") {
+        ($glyph, $hex, $char_name) = get_vars_from_hex($input);
     }
-    elsif($mode eq "--glyph") {
-        print_fonts($input);
+    elsif($mode eq "--char") {
+        ($glyph, $hex, $char_name) = get_vars_from_glyph($input);
+        $glyph = decode('UTF-8', $glyph);
     }
+    else {
+        pod2usage(1);
+    }
+    print_fonts($glyph, $hex, $char_name);
 }
 
 main(@ARGV);
+
+__END__
+=head1 NAME
+
+findfonts.pl - find fonts providing certain characters
+
+=head1 SYNOPSIS
+
+findfonts.pl [command] {hex,glyph}
+
+  Commands:
+    --hex         Search fonts by hex code.
+    --char        Search fonts by glyph.
+
+  Examples:
+    findfonts.pl --hex 1f49f
+    findfonts.pl --char 💟
+
+findfonts.pl version 0.1
+
+=cut
