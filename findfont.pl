e42e76e8a4ba370756627b0961d6b079a6dfda99
Rasmus Steinke <rasi@xssn.at>
2017-11-26 13:51:52 +0100

    use getops for cli args

--- findfont.pl
+++ findfont.pl
@@ -5,11 +5,19 @@ use autodie;
 use Data::Printer;
 use Encode qw/decode encode/;
 use Font::FreeType;
+use Getopt::Long;
 use charnames ();
 use Pod::Usage qw(pod2usage);
 
 my @font_list = map { (split /:/, $_)[0] } `fc-list`;
 @font_list = grep(/\.(ttf|odf)$/i, @font_list);
+
+my ($opt_hex, $opt_help, $opt_char);
+
+GetOptions ("hex=s"  => \$opt_hex,
+            "char=s" => \$opt_char,
+            "help"   => \$opt_help)
+or die("Invalid option. Try --help\n");
 
 sub print_fonts {
     my ($char, $hex, $char_name) = @_;
@@ -25,13 +33,11 @@ sub print_fonts {
         $face->set_char_size(0,0,0,0);
 
         my $glyph = $face->glyph_from_char_code(ord decode('UTF-8', $char));
-
         print "$font\n" if $glyph && $glyph->has_outline;
     }
 }
 
 sub get_vars_from_hex {
-    binmode(STDOUT, ":utf8");
     my ($hex) = @_;
     my $glyph = chr(hex($hex));
     my $char_name = charnames::viacode($hex);
@@ -47,22 +53,25 @@ sub get_vars_from_glyph {
     
 sub main {
     my ($glyph, $hex, $char_name);
-    my ($mode, $input) = @_;
 
-    if($mode eq "--hex") {
-        ($glyph, $hex, $char_name) = get_vars_from_hex($input);
+    if(defined $opt_hex) {
+        ($glyph, $hex, $char_name) = get_vars_from_hex($opt_hex);
     }
-    elsif($mode eq "--char") {
-        ($glyph, $hex, $char_name) = get_vars_from_glyph($input);
+    elsif(defined $opt_char) {
+        ($glyph, $hex, $char_name) = get_vars_from_glyph($opt_char);
         $glyph = decode('UTF-8', $glyph);
+    }
+    elsif(defined $opt_help) {
+        pod2usage(1);
     }
     else {
         pod2usage(1);
+        die;
     }
     print_fonts($glyph, $hex, $char_name);
 }
 
-main(@ARGV);
+main();
 
 __END__
 =head1 NAME
