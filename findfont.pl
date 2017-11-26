3b7c17366205aa9697d4cdceebbab07384085041
Rasmus Steinke <rasi@xssn.at>
2017-11-26 03:29:41 +0100

    print hex and decimal codes

--- findfont.pl
+++ findfont.pl
@@ -1,14 +1,22 @@
 use utf8;
 use autodie;
+use Data::Printer;
 
 use Encode qw/decode/;
 use Font::FreeType;
 
-my ($font, $char) = @ARGV;
+my @font_list = map { (split /:/, $_)[0] } `fc-list`;
+@font_list = grep(/\.(ttf|odf)$/i, @font_list);
+my $char = @ARGV[0];
+my $char_code_hex = sprintf "%04x", ord decode('UTF-8', $char);
+my $char_code_dec = sprintf "%04d", ord decode('UTF-8', $char);
 
-my $face = Font::FreeType->new->face($font);
-$face->set_char_size(0,0,0,0);
+foreach my $font (@font_list) {
+    my $face = Font::FreeType->new->face($font);
+    next unless $face->is_scalable;
+    $face->set_char_size(0,0,0,0);
 
-my $glyph = $face->glyph_from_char_code(ord decode('UTF-8', $char));
+    my $glyph = $face->glyph_from_char_code(ord decode('UTF-8', $char));
 
-print "Font has $char: $font\n" if $glyph && $glyph->has_outline;
+    print "Font has $char \(hex: $char_code_hex, dec: $char_code_dec\): $font\n" if $glyph && $glyph->has_outline;
+}
